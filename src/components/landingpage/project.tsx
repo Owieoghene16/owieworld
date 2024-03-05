e549387f31e4a7250e57343dd1cf3c4eb29fc11f
Owieoghene16 <kindnessokpugie1@gmail.com>
2024-03-05 18:46:29 +0100

    working on project container: part two

--- src/components/landingpage/project.tsx
+++ src/components/landingpage/project.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Projectone from '../../../public/projectone.png';
+import style from '../../app/style.module.css'
 
 
 export default function ProjectSection() {
@@ -13,13 +14,15 @@ export default function ProjectSection() {
 						  Project
 					  </h1>
 				  </div>
-          <div className="my-10 grid grid-cols-2">
-            <div>
-              <Image src={Projectone} alt='first-project' />
+          <div className="my-10 grid grid-cols-1 lg:grid-cols-2">
+            <div className="flex justify-center">
+              <div className="w-full lg:w-5/6">
+                <Image src={Projectone} alt='first-project' />
+              </div>
             </div>
             <div className="">
-              <h2 className="text-4xl font-medium">Suru</h2>
-							<div className="my-3 w-full md:w-3/4 border border-slate-200 py-8 px-6 shadow-inner">
+              <h2 className="text-4xl text-center lg:text-start font-medium">Suru</h2>
+							<div className="my-3 w-full lg:w-3/4 border border-slate-200 py-8 px-6 shadow-inner">
 								<span className="text-slate-500">Establish in 2010, and precisely registered under the laws of the Federal
 									 Republic of Nigeria in November 2010. Ronald and Partners Consult has sustained
 									  a good reputation in dispensing quality and professional immigration consulting
@@ -27,6 +30,32 @@ export default function ProjectSection() {
 										 in Nigeria and across the world.
 								</span>
 							</div>
+							<div className="mt-8 text-center lg:text-start">
+								<Link href='about'>
+								  <span className="text-white text-sm tracking-wide bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-full text-center  me-2 px-8 py-4">
+										Learn More
+									</span>
+								</Link>
+              </div>
+            </div>
+          </div>
+          <div className={style.project__border}></div>
+          <div className="my-10 grid grid-cols-1 lg:grid-cols-2">
+            <div className="">
+              <h2 className="text-4xl text-center lg:text-right font-medium w-full lg:w-3/4">Suru</h2>
+							<div className="my-3 w-full lg:w-3/4 border border-slate-200 py-8 px-6 shadow-inner">
+								<span className="text-slate-500">Establish in 2010, and precisely registered under the laws of the Federal
+									 Republic of Nigeria in November 2010. Ronald and Partners Consult has sustained
+									  a good reputation in dispensing quality and professional immigration consulting
+										 and overseas education placement services to clients and prospective students 
+										 in Nigeria and across the world.
+								</span>
+							</div>
+            </div>
+            <div className="flex justify-center">
+              <div className="w-full lg:w-5/6">
+                <Image src={Projectone} alt='first-project' />
+              </div>
             </div>
           </div>
         </div>
